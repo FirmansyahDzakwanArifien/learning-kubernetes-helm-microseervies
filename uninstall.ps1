f6abf899b2984665f08e3bfea3693a085359d8de
Firmansyah Dzakwan Arifien <fdzak01@gmail.com>
2025-09-21 09:01:46 +0700

    Add all file

---
+++ uninstall.ps1
@@ -0,0 +1,12 @@
+helm uninstall redis-cart
+
+helm uninstall emailservice
+helm uninstall cartservice
+helm uninstall currencyservice
+helm uninstall paymentservice
+helm uninstall recommendationservice
+helm uninstall productcatalogservice
+helm uninstall shippingservice
+helm uninstall adservice
+helm uninstall checkoutservice
+helm uninstall frontend
